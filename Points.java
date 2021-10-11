c668185492ed92da1a400150df17f3ee8e3ec809
Sohom2014 <40815603+Sohom2014@users.noreply.github.com>
2021-10-11 18:30:53 +0530

    Add files via upload

---
+++ Points.java
@@ -0,0 +1,18 @@
+package com.basics;
+
+public class Points {
+
+		public int x,y;
+	  Points(int i,int j)    //Assignment 3.1
+		{
+			x=i;
+		  y=j;
+		}
+		public void Answer()  //Assignment 3.2
+		{
+			
+	System.out.println("The result is:"+x + " " +y);
+	}
+		
+	}
+
